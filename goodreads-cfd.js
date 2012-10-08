93ee13921f30102bf34070ba10f40fe6b436c07e
Marcin Floryan <marcin.floryan@gmail.com>
2012-10-08 22:24:07 +0100

    An application object in the js

--- goodreads-cfd.js
+++ goodreads-cfd.js
@@ -1,13 +1,20 @@
-var cfdData = cfdData || {};
+var application = (function() {
+    var cfdData = {};
+    var pub = {};
+
+    pub.showFormIfBrowserDataNoAvailable = function () {
+        var goodreadsData = localStorage.getItem("mmsquare.goodreads-cdn.books");
+        if (goodreadsData) {
+            cfdData.data = goodreadsData;
+        } else {
+            $('#goodreads-api').show();
+        }
+    };
+
+    return pub;
+}());
+
 
 $(document).ready(function() {
-
-    var goodreadsData = localStorage.getItem("mmsquare.goodreads-cdn.books");
-    if (goodreadsData) {
-        cfdData.data = goodreadsData;
-    } else {
-        $('#goodreads-api').show();
-    }
-
-
+    application.showFormIfBrowserDataNoAvailable();
 });
